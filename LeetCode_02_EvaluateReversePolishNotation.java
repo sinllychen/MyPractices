f0b0ba913572cb37c4aa4ec70915958db3c85e6e
sinllychen <sinllychenfjnp@163.com>
2014-05-08 17:19:58 +0800

    LeetCode_02_EvaluateReversePolishNotation.java

---
+++ LeetCode_02_EvaluateReversePolishNotation.java
@@ -0,0 +1,53 @@
+public class LeetCode_02_EvaluateReversePolishNotation {
+    public int evalRPN(String[] tokens) {
+       Stack<Integer> s=new Stack<Integer>();
+		int num=0;
+        for(int i=0;i<tokens.length;i++)
+        {
+        		try
+        		{
+        			int a=Integer.parseInt(tokens[i]);
+        			s.push(a);
+        		}
+        		catch(Exception e)
+        		{
+        			if(tokens[i].equals("+"))
+        			{
+        				int a=s.pop();
+        				int b=s.pop();
+        				num=a+b;
+        				s.push(num);
+        			}
+        		   if(tokens[i].equals("-"))
+        		   {
+        			   int a=s.pop();
+       			       int b=s.pop();
+       			    num=b-a;
+    				s.push(num);
+        		   }
+        		   if(tokens[i].equals("*"))
+        		   {
+        			   int a=s.pop();
+       			       int b=s.pop();
+       			    num=a*b;
+    				s.push(num);
+        		   }
+        		   if(tokens[i].equals("/"))
+        		   {
+        			   int a=s.pop();
+       			       int b=s.pop();
+       			    num=b/a;
+    				s.push(num);
+        		   }
+        		}
+        }
+        if(!s.isEmpty())
+        	num=s.pop();
+        return num;
+    }
+    public static void main(String[] args)
+    {
+        String[] tokens={"2", "1", "+", "3", "*"};
+        System.out.println(evalRPN(tokens));
+    }
+}
