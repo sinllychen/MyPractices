1c6d10df44b7dd3357888226393ec78a20faa0d6
sinllychen <sinllychenfjnp@163.com>
2014-05-10 19:58:14 +0800

    Update LeetCode_02.java

--- LeetCode_02.java
+++ LeetCode_02.java
@@ -1,48 +1,48 @@
 public class LeetCode_02 {
     public int evalRPN(String[] tokens) {
        Stack<Integer> s=new Stack<Integer>();
-		int num=0;
-        for(int i=0;i<tokens.length;i++)
-        {
-        		try
-        		{
-        			int a=Integer.parseInt(tokens[i]);
-        			s.push(a);
-        		}
-        		catch(Exception e)
-        		{
-        			if(tokens[i].equals("+"))
-        			{
-        				int a=s.pop();
-        				int b=s.pop();
-        				num=a+b;
-        				s.push(num);
-        			}
-        		   if(tokens[i].equals("-"))
-        		   {
-        			   int a=s.pop();
-       			       int b=s.pop();
-       			    num=b-a;
-    				s.push(num);
-        		   }
-        		   if(tokens[i].equals("*"))
-        		   {
-        			   int a=s.pop();
-       			       int b=s.pop();
-       			    num=a*b;
-    				s.push(num);
-        		   }
-        		   if(tokens[i].equals("/"))
-        		   {
-        			   int a=s.pop();
-       			       int b=s.pop();
-       			    num=b/a;
-    				s.push(num);
-        		   }
-        		}
+       int num=0;
+       for(int i=0;i<tokens.length;i++)
+       {
+            try
+            {
+                int a=Integer.parseInt(tokens[i]);
+                s.push(a);
+            }
+            catch(Exception e)
+            {
+        	if(tokens[i].equals("+"))
+        	{
+        	    int a=s.pop();
+        	    int b=s.pop();
+        	    num=a+b;
+        	    s.push(num);
+        	}
+                if(tokens[i].equals("-"))
+                {
+        	    int a=s.pop();
+       	            int b=s.pop();
+       	            num=b-a;
+    		    s.push(num);
+                }
+               if(tokens[i].equals("*"))
+               {
+        	    int a=s.pop();
+       	            int b=s.pop();
+       		    num=a*b;
+    		    s.push(num);
+               }
+               if(tokens[i].equals("/"))
+               {
+        	    int a=s.pop();
+       	            int b=s.pop();
+       		    num=b/a;
+    	            s.push(num);
+               }
+            }
         }
         if(!s.isEmpty())
-        	num=s.pop();
+            num=s.pop();
         return num;
     }
     public static void main(String[] args)
